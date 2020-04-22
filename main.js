6ccee0d7a68eb77e13c026717084555cef879034
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2020-04-22 17:23:34 -0500

    ChickenMonkey exercise

---
+++ main.js
@@ -0,0 +1,16 @@
+
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+
+    if (currentNumber % 35 === 0 ) {
+        console.log("ChickenMonkey");
+    }
+    
+    else if (currentNumber % 5 === 0) {
+        console.log("Chicken"); 
+    }
+    else if (currentNumber % 7 ===0) {
+        console.log("Monkey");
+    } else {
+    console.log(currentNumber);
+    }
+}
